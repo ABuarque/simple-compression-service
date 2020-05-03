b9838bae9a2d39b0b9ba9bf01248838ab7ecc0df
abuarquemf <abuarquemf@gmail.com>
2020-05-03 14:00:51 -0300

    removed unused method and comment

--- src/apps/inputhandler/main.go
+++ src/apps/inputhandler/main.go
@@ -8,7 +8,6 @@ import (
 	amassa "inputhandler/proto"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/ABuarque/simple-compression-service/src/libs/redis"
@@ -26,48 +25,11 @@ type in struct {
 
 var re *redis.Client
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var in in
-	err := json.NewDecoder(r.Body).Decode(&in)
-	if err != nil {
-		log.Println("error decoing payload from request: ", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		payload := map[string]string{
-			"msg": "failed to decode file",
-		}
-		json.NewEncoder(w).Encode(payload)
-	}
-	payloadAsBytes, err := json.Marshal(in)
-	if err != nil {
-		log.Println("error marshaling payload to pusblish: ", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		payload := map[string]string{
-			"msg": "failed to decode file",
-		}
-		json.NewEncoder(w).Encode(payload)
-	}
-	payload := make(map[string]string)
-	if in.Command == "compress" {
-		re.Publish("compression", string(payloadAsBytes))
-		log.Println("published compression topic")
-		payload["msg"] = "file being compressed!"
-	} else {
-		re.Publish("decompression", string(payloadAsBytes))
-		log.Println("published decompression topic")
-		payload["msg"] = "file being decompressed!"
-	}
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(payload)
-}
-
 // Server is according the interface contract
 type Server struct{}
 
 // RequestAction is a
 func (s *Server) RequestAction(ctx context.Context, req *amassa.Request) (*amassa.Response, error) {
-	fmt.Println("Chegou oia ", req)
-	fmt.Println("name: ", req.GetName())
 	in := in{
 		File:    req.GetBytes(),
 		Email:   req.GetEmail(),
@@ -84,13 +46,12 @@ func (s *Server) RequestAction(ctx context.Context, req *amassa.Request) (*amass
 		return &amassa.Response{
 			Status: 2,
 		}, nil
-	} else {
-		re.Publish("decompression", string(payloadAsBytes))
-		log.Println("published decompression topic")
-		return &amassa.Response{
-			Status: 2,
-		}, nil
 	}
+	re.Publish("decompression", string(payloadAsBytes))
+	log.Println("published decompression topic")
+	return &amassa.Response{
+		Status: 2,
+	}, nil
 }
 
 func main() {
