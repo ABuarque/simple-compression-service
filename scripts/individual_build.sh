130bffe6b71919d7c9094fd342a2b2c02e786b01
abuarquemf <abuarquemf@gmail.com>
2020-05-03 16:07:22 -0300

    build with no cache

--- scripts/individual_build.sh
+++ scripts/individual_build.sh
@@ -8,6 +8,6 @@ image="${REPO_PREFIX}/$service:$tag"
 
 cd ../src/apps/$service 
 
-docker build -t "${image}" --build-arg ACCESS_TOKEN_USR="${GITHUB_USER}" --build-arg ACCESS_TOKEN_PWD="${GITHUB_TOKEN}" . 
+docker build --no-cache -t "${image}" --build-arg ACCESS_TOKEN_USR="${GITHUB_USER}" --build-arg ACCESS_TOKEN_PWD="${GITHUB_TOKEN}" . 
 
 docker push "${image}"
